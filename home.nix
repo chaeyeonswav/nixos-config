12a61c8dea60354008720b3928695fb9401a5bbd
koehnnnn <koehn40308@gmail.com>
2024-02-16 01:31:31 -0500

    cleaned it up

--- home.nix
+++ home.nix
@@ -3,20 +3,24 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "koehn";
-  home.homeDirectory = "/home/koehn";
+  home = {
+    username = "koehn";
+    homeDirectory = "/home/koehn";
+    stateVersion = "23.11";
+  };
 
-  programs.git = {
+  programs = {
+    git = {
       enable = true;
       userName = "koehnnnn";
       userEmail = "koehn40308@gmail.com";
     };
 
-    programs.firefox.enable = true;
+    firefox.enable = true;
+    nushell.enable = true;
+    home-manager.enable = true;
 
-    programs.nushell.enable = true;
-
-    programs.vscode = {
+    vscode = {
       enable = true;
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
@@ -43,18 +47,5 @@
         git.autofetch = true;
       };
     };
-
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  };
 }
