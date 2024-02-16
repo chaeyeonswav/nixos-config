58e00cd54d06980b2e41cd22abdd0a23c7c009ad
koehnnnn <koehn40308@gmail.com>
2024-02-16 01:11:33 -0500

    Init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My system";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows =
+        "nixpkgs"; # Use system packages list where available
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.aesthetic = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users = {
+                koehn = import ./home.nix;
+              };
+            };
+          }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "koehn";
+  home.homeDirectory = "/home/koehn";
+
+  programs.git = {
+      enable = true;
+      userName = "koehnnnn";
+      userEmail = "koehn40308@gmail.com";
+    };
+
+    programs.firefox.enable = true;
+
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        pkief.material-icon-theme
+      ];
+      userSettings = {
+        editor.formatOnSave = true;
+        nix.enableLanguageServer = true;
+        workbench.iconTheme = "material-icon-theme";
+        git.autofetch = true;
+      };
+    };
+
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
